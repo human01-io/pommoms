58b5f9c9def29868262d8a924425ea89772f8c7b
human01-io <98203797+human01-io@users.noreply.github.com>
2025-05-06 07:37:02 +0000

    v1.02

--- pommoms/components/WaitlistForm.tsx
+++ pommoms/components/WaitlistForm.tsx
@@ -18,7 +18,7 @@ const WaitlistForm = () => {
       // For demonstration purposes, let's just show success
       setStatus('success');
       setEmail('');
-    } catch (error) {
+    } catch (_) {
       setStatus('error');
     }
   };
